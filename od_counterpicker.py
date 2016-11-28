08f084bbad9b2cc1a00ecc12c4f91dfca990bc20
neffers <nefkrystal@gmail.com>
2016-11-28 17:07:41 -0500

    actually i just rewrote it

--- od_counterpicker.py
+++ od_counterpicker.py
@@ -24,47 +24,64 @@ def getWinrates(account,againstHeroID=None,radiant=None):
         winrates[int(hero['hero_id'])]['id'] = int(hero['hero_id'])
     return winrates
 
-def getNextTopWinrate(winrates,sortKey='winrate'):
+def getNextTop(winrates,sortKey='winrate'):
     return winrates.pop(max(winrates.keys(),key=lambda k:winrates[k][sortKey]))
 
+def getWinratesWithDeltas(account,againstHeroID,radiant=None):
+    #essentially returns a general winrates dict with deltas for the given hero appended
+    winrates = getWinrates(account,None,radiant)
+    winratesAgainstAntagonist = getWinrates(account,againstHeroID,radiant)
+    for hero in winrates:
+        try:
+            winrates[hero]['winrateDelta'] = winratesAgainstAntagonist[hero]['winrate'] - winrates[hero]['winrate']
+        except TypeError:
+            winrates[hero]['winrateDelta'] = 0
+    return winrates
+
+def avgDeltas(winrates1,winrates2,multiplierForWinrates1=1):
+    #modifies the winrates1 dict to average winrateDelta of both passed dicts
+    for hero in winrates1:
+        winrates1[hero]['winrateDelta'] = (winrates1[hero]['winrateDelta'] * multiplierForWinrates1 + winrates2[hero]['winrateDelta'])/(multiplierForWinrates1+1)
+
 def printTopHeroes(winrates,sortKey='winrate',numHeroes=3):
+    #prints the top n heroes by creating a copy of the dict passed in, 
     winratesCopy = winrates
     for num in range(numHeroes):
-        hero = getNextTopWinrate(winratesCopy,sortKey)
+        hero = getNextTop(winratesCopy,sortKey)
         if not hero[sortKey] == None and hero[sortKey] > 0:
             print "#"+str(num+1)+": "+dotaTools.translateHeroID(hero['id']),hero[sortKey]
 
-def getWinratesDelta(account,againstHeroID,radiant=None):
-    generalWinrates = getWinrates(account,None,radiant)
-    specificWinrates = getWinrates(account,againstHeroID,radiant)
-    winrateDeltas = {}
-    for heroID in generalWinrates:
-        winrateDeltas[heroID] = {'id':heroID,'generalWinrate':generalWinrates[heroID]['winrate'],'totalGamesOnHero':generalWinrates[heroID]['games'],'gamesAgainstAntagonist':specificWinrates[heroID]['games']}
-        try:
-            winrateDeltas[heroID]['winrateDelta'] = specificWinrates[heroID]['winrate'] - generalWinrates[heroID]['winrate']
-        except TypeError:
-            winrateDeltas[heroID]['winrateDelta'] = None
-        try:
-#            winrateDeltas[heroID]['weightedWinrate'] = (generalWinrates[heroID]['winrate'] * specificWinrates[heroID]['games'] + winrateDeltas[heroID]['winrateDelta'] * (generalWinrates[heroID]['games']-specificWinrates[heroID]['games'])) / generalWinrates[heroID]['games']
-            winrateDeltas[heroID]['weightedWinrate'] = (generalWinrates[heroID]['winrate'] * (generalWinrates[heroID]['games']-specificWinrates[heroID]['games']) + winrateDeltas[heroID]['winrateDelta'] * specificWinrates[heroID]['games']) / generalWinrates[heroID]['games']
-        except TypeError:
-            winrateDeltas[heroID]['weightedWinrate'] = None
-    return winrateDeltas
-
-def avgWinrateDeltas(winrate1,winrate2,iterationsOfWinrate1=0):
-    averagedWinrates = {}
-    for heroID in winrate1:
-        averagedWinrates[heroID] = {'id':heroID,'generalWinrate':winrate1[heroID]['generalWinrate'],'totalGamesOnHero':winrate1[heroID]['totalGamesOnHero']}
-        try:
-            averagedWinrates[heroID]['weightedWinrate'] = (winrate1[heroID]['weightedWinrate']*(iterationsOfWinrate1+1) + winrate2[heroID]['weightedWinrate']) / (iterationsOfWinrate1+2)
-        except TypeError:
-            averagedWinrates[heroID]['weightedWinrate'] = None
-        try:
-            averagedWinrates[heroID]['winrateDelta'] = (winrate1[heroID]['winrateDelta']*(iterationsOfWinrate1+1) + winrate2[heroID]['winrateDelta']) / (iterationsOfWinrate1+2)
-        except TypeError:
-            averagedWinrates[heroID]['winrateDelta'] = None
-    return averagedWinrates
-
+#def getWinratesDelta(account,againstHeroID,radiant=None):
+#    generalWinrates = getWinrates(account,None,radiant)
+#    specificWinrates = getWinrates(account,againstHeroID,radiant)
+#    winrateDeltas = {}
+#    for heroID in generalWinrates:
+#        winrateDeltas[heroID] = {'id':heroID,'generalWinrate':generalWinrates[heroID]['winrate'],'totalGamesOnHero':generalWinrates[heroID]['games'],'gamesAgainstAntagonist':specificWinrates[heroID]['games']}
+#        try:
+#            winrateDeltas[heroID]['winrateDelta'] = specificWinrates[heroID]['winrate'] - generalWinrates[heroID]['winrate']
+#        except TypeError:
+#            winrateDeltas[heroID]['winrateDelta'] = None
+#        try:
+##            winrateDeltas[heroID]['weightedWinrate'] = (generalWinrates[heroID]['winrate'] * specificWinrates[heroID]['games'] + winrateDeltas[heroID]['winrateDelta'] * (generalWinrates[heroID]['games']-specificWinrates[heroID]['games'])) / generalWinrates[heroID]['games']
+#            winrateDeltas[heroID]['weightedWinrate'] = (generalWinrates[heroID]['winrate'] * (generalWinrates[heroID]['games']-specificWinrates[heroID]['games']) + winrateDeltas[heroID]['winrateDelta'] * specificWinrates[heroID]['games']) / generalWinrates[heroID]['games']
+#        except TypeError:
+#            winrateDeltas[heroID]['weightedWinrate'] = None
+#    return winrateDeltas
+#
+#def avgWinrateDeltas(winrate1,winrate2,iterationsOfWinrate1=0):
+#    averagedWinrates = {}
+#    for heroID in winrate1:
+#        averagedWinrates[heroID] = {'id':heroID,'generalWinrate':winrate1[heroID]['generalWinrate'],'totalGamesOnHero':winrate1[heroID]['totalGamesOnHero']}
+#        try:
+#            averagedWinrates[heroID]['weightedWinrate'] = (winrate1[heroID]['weightedWinrate']*(iterationsOfWinrate1+1) + winrate2[heroID]['weightedWinrate']) / (iterationsOfWinrate1+2)
+#        except TypeError:
+#            averagedWinrates[heroID]['weightedWinrate'] = None
+#        try:
+#            averagedWinrates[heroID]['winrateDelta'] = (winrate1[heroID]['winrateDelta']*(iterationsOfWinrate1+1) + winrate2[heroID]['winrateDelta']) / (iterationsOfWinrate1+2)
+#        except TypeError:
+#            averagedWinrates[heroID]['winrateDelta'] = None
+#    return averagedWinrates
+#
 def getFiveHeroes():
     account32 = steamTools.getUserInput32withShortcuts()
     aggregateWinrates = {}
@@ -82,9 +99,9 @@ def getFiveHeroes():
     for heronum in range(5):
         heroes.append(dotaTools.parseHeroName(raw_input("Opponent hero #"+str(heronum+1)+": ")))
         if len(heroes) > 1:
-            aggregateWinrates = avgWinrateDeltas(aggregateWinrates,getWinratesDelta(account32,heroes[-1],side),heronum)
+            avgDeltas(aggregateWinrates,getWinratesWithDeltas(account32,heroes[-1],side),heronum)
         else:
-            aggregateWinrates = getWinratesDelta(account32,heroes[0],side)
+            aggregateWinrates = getWinratesWithDeltas(account32,heroes[0],side)
         printTopHeroes(aggregateWinrates,'winrateDelta',10)
 
 getFiveHeroes()
